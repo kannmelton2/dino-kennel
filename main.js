c9fa604c4d729dd388a178cb8f34f2d3750cdcaa
Kayla Melton <kannmelton2@gmail.com>
2020-02-22 11:29:43 -0600

    added create dinos and began single view

--- main.js
+++ main.js
@@ -1,4 +1,82 @@
-const dinos = [];
+const dinos = [
+    {
+      id: 'dino1',
+      name: 'Rex',
+      type: 'T Rex',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 100,
+      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+      id: 'dino2',
+      name: 'Steve',
+      type: 'Velociraptor',
+      age: 1,
+      owner: 'Mary',
+      adventures: [],
+      health: 100,
+      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    },
+    {
+      id: 'dino3',
+      name: 'Susan',
+      type: 'Stegasaurus',
+      age: 55,
+      owner: 'Luke',
+      adventures: [],
+      health: 100,
+      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+    }
+  ];
+
+// PRINT TO DOM
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+const viewSingleDino = () => {
+    let domString = '';
+    domString += '<button id="close-single-view" class="btn btn-outline-dark single-dino"><i class="fas fa-times"></i></button>';
+    printToDom('kennel', '');
+    printToDom('single-view', domString);
+    document.getElementById('close-single-view').addEventListener('click', closeSingleViewEvent);
+};
+
+const closeSingleViewEvent = () => {
+    printToDom('single-view', '');
+    printDinos(dinos);
+};
+
+
+// EVENT LISTENER for viewing single dino -- need to creat viewSingleDino func
+const singleDinoAddEvents = () => {
+    const dinoViewButtons = document.getElementsByClassName('single-dino');
+    for (let i = 0; i < dinoViewButtons.length; i++) {
+        dinoViewButtons[i].addEventListener('click', viewSingleDino);
+    }
+};
+
+// PRINT DINOS FUNC
+const printDinos = (dinoArray) => {
+    let domString = '';
+    for (let i =0; i < dinoArray.length; i++){
+      domString += '<div class="col-4">';
+      domString += '<div class="card">';
+      domString += `<img class="card-img-top" src="${dinoArray[i].imageUrl}" alt="Card image cap">`;
+      domString += '<div class="card-body">';
+      domString += `  <h5 class="card-title">${dinoArray[i].name}</h5>`;
+      domString += `  <p class="card-text">Health: ${dinoArray[i].health}</p>`;
+      domString += '<button class="btn btn-outline-dark single-dino"><i class="fas fa-eye"></i></button>';
+      domString += '</div>';
+      domString += '</div>';
+      domString += '</div>';
+    }
+    printToDom('kennel', domString);
+    singleDinoAddEvents();
+  };
 
 // CREATE DINO IN ACCORDION FORM
 const newDino = (e) => {
@@ -15,13 +93,14 @@ const newDino = (e) => {
             imageUrl: document.getElementById('dino-image').value
         }
     dinos.push(brandNewDino);
-    console.log('hi from newDino', dinos);
     document.getElementById('new-dino-form').reset();
     document.getElementById('collapseOne').classList.remove('show');
+    printDinos(dinos);
 };
 
 
 const init = () => {
+    printDinos(dinos);
     document.getElementById('submit-new-dino').addEventListener('click', newDino);
 };
 
