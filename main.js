2e522506e47eedb4673c4036f7572b56d6a3d86f
Kayla Melton <kannmelton2@gmail.com>
2020-02-22 10:21:34 -0600

    reset form

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('dinos');
+const dinos = [];
+
+// CREATE DINO IN ACCORDION FORM
+const newDino = (e) => {
+    e.preventDefault();
+    const brandNewDino =
+        {
+            id: `dino${dinos.length + 1}`,
+            name: document.getElementById('dino-name').value,
+            type: document.getElementById('dino-type').value,
+            age: document.getElementById('dino-age').value,
+            owner: document.getElementById('dino-owner').value,
+            adventures: [],
+            health: 100,
+            imageUrl: document.getElementById('dino-image').value
+        }
+    dinos.push(brandNewDino);
+    document.getElementById('new-dino-form').reset();
+};
+
+const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
+};
+
+init();
