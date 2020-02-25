b14f73e18db1bf9e5b16f68a7ef2f45281c19f4b
Kayla Melton <kannmelton2@gmail.com>
2020-02-25 14:06:12 -0600

    added hospital

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const dinos = [
       age: 100,
       owner: 'Zoe',
       adventures: [],
-      health: 99,
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+      health: 92,
+      imageUrl: 'https://www.fieldandstream.com/resizer/8xkluKAxQZsEHJKj6qwyU0mLhTo=/760x448/filters:focal(458x270:459x271)/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/TQFN3CD5DAEM4DL2ACD42ZJ5E4.png'
     },
     {
       id: 'dino2',
@@ -17,17 +17,77 @@ const dinos = [
       owner: 'Mary',
       adventures: [],
       health: 1,
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+      imageUrl: 'https://i.ebayimg.com/images/g/61UAAOSweNpdmtI2/s-l640.png'
     },
     {
       id: 'dino3',
       name: 'Susan',
-      type: 'Stegasaurus',
+      type: 'stegosaurus',
       age: 55,
       owner: 'Luke',
       adventures: [],
-      health: 45,
-      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/61fC04pumjL._AC_SL1001_.jpg'
+      health: 0,
+      imageUrl: 'https://cdn.mos.cms.futurecdn.net/owYTb9X5fKpeBhgiaxD73b-320-80.jpg'
+    },
+    {
+      id: 'dino4',
+      name: 'Barry',
+      type: 'Brontosaurus',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 100,
+      imageUrl: 'https://images.unsplash.com/photo-1517923368558-70ca9ac84b39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80'
+    },
+    {
+      id: 'dino5',
+      name: 'Steph',
+      type: 'Spinosaurus',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 75,
+      imageUrl: 'https://cdn1.bigcommerce.com/n-yp39j5/ujq6o/products/1060/images/2390/Papo_Spinosaurus_2019_DansDinosaurs__69805.1552618774.1280.1280.jpg?c=2'
+    },
+    {
+      id: 'dino6',
+      name: 'Tim',
+      type: 'Talarurus',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 55,
+      imageUrl: 'https://vignette.wikia.nocookie.net/dinosaurs/images/2/2b/TalarurusInfobox.png/revision/latest/scale-to-width-down/340?cb=20150512165226'
+    },
+    {
+      id: 'dino7',
+      name: 'Tracy',
+      type: 'Triceratops',
+      age: 100,
+      owner: 'Zoe',
+      adventures: [],
+      health: 0,
+      imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/81Wsvp2M7iL._AC_SX425_.jpg'
+    },
+    {
+      id: 'dino8',
+      name: 'Percy',
+      type: 'Pterodactyl',
+      age: 10,
+      owner: 'Mary',
+      adventures: [],
+      health: 10,
+      imageUrl: 'https://images.dinosaurpictures.org/3_pterodactyl_63be.jpg'
+    },
+    {
+      id: 'dino9',
+      name: 'Betty',
+      type: 'brontosaurus',
+      age: 22,
+      owner: 'Zoe',
+      adventures: [],
+      health: 22,
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTOdrC7hlvBawFQ7g8vgwHcfQphX5WfeN2bth0dvc4M2oxNGdSD'
     }
   ];
 
@@ -35,7 +95,18 @@ const dinos = [
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
-}
+};
+
+// CLOSE SINGLE VIEW
+const closeSingleViewEvent = () => {
+    printToDom('single-view', '');
+    printDinos(dinos);
+};
+
+// ADVENTURE TABLE BUILDER
+// const adventureTableBuilder = (advArray) => {
+
+// };
 
 // VIEW SINGLE DINO
 const viewSingleDino = (e) => {
@@ -64,11 +135,7 @@ const viewSingleDino = (e) => {
     document.getElementById('close-single-view').addEventListener('click', closeSingleViewEvent);
 };
 
-// CLOSE SINGLE VIEW
-const closeSingleViewEvent = () => {
-    printToDom('single-view', '');
-    printDinos(dinos);
-};
+
 
 
 // EVENT LISTENER for viewing single dino
@@ -125,6 +192,7 @@ const feedMe = (e) => {
     };
 };
 
+// FEED DINOS EVENT
 const feedEvents = () => {
     const dinoFeedButtons = document.getElementsByClassName('feed-dino');
     for (let i = 0; i < dinoFeedButtons.length; i++) {
@@ -134,7 +202,7 @@ const feedEvents = () => {
 
 
 // PRINT DINOS FUNC
-const printDinos = (dinoArray) => {
+const printDinos = (dinoArray, divId) => {
     let domString = '';
     for (let i =0; i < dinoArray.length; i++){
       domString += '<div class="col-4">';
@@ -152,7 +220,7 @@ const printDinos = (dinoArray) => {
       domString += '</div>';
       domString += '</div>';
     }
-    printToDom('kennel', domString);
+    printToDom(divId, domString);
     singleDinoAddEvents();
     petEvents();
     deleteEvents();
@@ -176,12 +244,29 @@ const newDino = (e) => {
     dinos.push(brandNewDino);
     document.getElementById('new-dino-form').reset();
     document.getElementById('collapseOne').classList.remove('show');
-    printDinos(dinos);
+    buildDinos(dinos);
 };
+
+// FIND HOSPITAL DINOS
+const findHospitalDinos = () => {
+    const hospitalDinos = dinos.filter((x) => x.health > 0 && x.health < 40);
+    printDinos(hospitalDinos, 'hospital');
+};
+
+// FIND LIVE HEALTHY DINOS
+const findLiveHealthyDinos = () => {
+    const liveHealthyDinos = dinos.filter((x) => x.health > 39);
+    printDinos(liveHealthyDinos, 'kennel');
+};
+
+const buildDinos = () => {
+    findHospitalDinos();
+    findLiveHealthyDinos();
+}
 
 
 const init = () => {
-    printDinos(dinos);
+    buildDinos();
     document.getElementById('submit-new-dino').addEventListener('click', newDino);
 };
 
